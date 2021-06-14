f60f633550b72604edde3a86c959280592e57a38
hyejinpark <phj95304@gmail.co>
2021-06-15 01:29:22 +0900

    question extractioin complete

--- Q_extractor_mathQA.py
+++ Q_extractor_mathQA.py
@@ -8,39 +8,44 @@ from googletrans import Translator
 def dumper(question):
     global idx
     global data
-    print(question)
+    #print(question)
     data.append({str(idx) : {"question":question}})
 
-    with open("./answersheet_MATHQA.json", "w", encoding="utf-8") as testfile:
+    with open("./questionsheet_MATHQA.json", "w", encoding="utf-8") as testfile:
         json.dump(data, testfile, ensure_ascii=False)
 
 
-def extractor(json_file_path, ENG_question_file_path):
+# def extractor(json_file_path): json file load and translate
+#     global idx
+#     json_file = open(json_file_path)
+#     data = json.load(json_file)
+
+#     for entity in data:
+#         question = entity["Problem"]
+
+#         translator = Translator()
+#         trans = translator.translate(question, src='en', dest='ko')
+    
+#         #write the problem from MATHQA in KOR json type 
+#         #print(trans.text)
+#         dumper(trans.text)
+#         idx+=1
+
+#한국어로 번역된 파일을 이용해 json 생성
+def extractor(text_file_path):
     global idx
-    json_file = open(json_file_path)
-    question_file = open(question_file_path, "w")
-    data = json.load(json_file)
-
-    for entity in data:
-        question = entity["Problem"]
-
-        translator = Translator()
-        trans = translator.translate(question, src='en', dest='ko')
-        #wrtite the problem from MATHQA in English
-        question_file.write(question)
-        question_file.write("\n")
-        #write the problem from MATHQA in KOR json type 
-        #print(trans.text)
-        dumper(trans.text)
+    data = open(text_file_path)
+    for i in data:
+        dumper(i)
         idx+=1
+
 
 
  
 if __name__ == "__main__":
     json_file_path = "./mathQA.json"
-    question_file_path = "./mathQA_answer.json"
-
+    text_file_path = "./question_KOR.txt"
     idx = 1
     data = []
-    extractor(json_file_path, question_file_path)
-    jsonMaker(question_file_path)
+    extractor(text_file_path)
+    
